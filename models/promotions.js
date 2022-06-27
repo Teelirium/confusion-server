e5f05ac2988598bf6d7c27fa5df93b473867d6e9
Teelirium <angryzombi01@gmail.com>
2022-06-27 20:27:44 +0500

    ts fix

--- models/promotions.js
+++ models/promotions.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 require('mongoose-currency').loadType(mongoose);
+//@ts-ignore
 const Currency = mongoose.Types.Currency;
 
 const promoSchema = new Schema({
